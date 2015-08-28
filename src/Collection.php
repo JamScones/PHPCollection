8ec1fc88eaec9e3abe815b65a2a20c8c58b0b901
Sam Jones <sam@Sams-MacBook-Air.local>
2015-08-28 18:18:06 +0100

    Huh. Including the class file might help ...

---
+++ src/Collection.php
@@ -0,0 +1,22 @@
+<?php
+
+class Collection {
+	private $collectiondata;
+	public function __construct(){
+		
+	}
+
+	public function parseFromCSVData($csv){
+		// this will fail for quoted multiline values in CSV, update to http://csv.thephpleague.com/ sometime?
+		$lines = explode(PHP_EOL, $csv);
+		$collectiondata = new array();
+		foreach($lines as $line){
+			$collectiondata[] = str_getcsv($line);
+		}
+		
+	}
+
+
+}
+
+?>
